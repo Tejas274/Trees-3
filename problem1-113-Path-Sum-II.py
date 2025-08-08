b4fc32da07f73120e2c71a7037cf0518354ffb1b
tejas274 <shahtejas274@gmail.com>
2025-08-07 20:02:39 -0400

    problem 1 added

---
+++ problem1-113-Path-Sum-II.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#Time Complexity : o(n)
+#Space Complexity : o(h) + o(n*h)
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+
+        self.result = []
+        self.target = targetSum
+        self.helper(root, [], 0)
+        return self.result
+
+    def helper(self, root: Optional[TreeNode], list1: list, currSum: int) -> None:
+
+        if root == None:
+            return
+
+        newSum = currSum + root.val
+        newList = list1 + [root.val]
+
+        if root.left == None and root.right == None:
+            if newSum == self.target:
+                self.result.append(newList)
+                return
+
+        self.helper(root.left, newList, newSum)
+        self.helper(root.right, newList, newSum)
+
+
+## pop way remaning
